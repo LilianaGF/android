beb4390ab3b4421ca5d3a89e5ea5f549d15b531b
Liliana Gutierrez <lilianagf.750@gmail.com>
2018-06-05 19:22:34 -0500

    Ejercicio 7. Mostrar lista de tareas.
    Signed-off-by: Liliana Gutierrez <lilianagf.750@gmail.com>

--- app/src/main/java/towa/task_app/TaskListActivity.java
+++ app/src/main/java/towa/task_app/TaskListActivity.java
@@ -1,52 +1,42 @@
 package towa.task_app;
-
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.View;
-
+import android.widget.TextView;
+import android.widget.Toast;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity
+public class TaskListActivity extends AppCompatActivity
 {
     //                                                      //tag for Log.d() method.
     final static String tag = "LGF";
     BroadcastReceiver showTaskReceiver = new ShowTaskReceiver();
+
     //------------------------------------------------------------------------------------------------------------------
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    //                                                      //Called when the activity is first created.
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Log.d(tag, "The onCreate() event");
-        //                                                  //To register a receiver of the broadcast
-        Log.d(tag, "Registrando....");
+        setContentView(R.layout.activity_task_list);
 
+        //                                                  //To register a receiver of the broadcast
         IntentFilter intentFilter = new IntentFilter("com.LGF.CUSTOM_INTENT.TasksReady");
         this.registerReceiver(this.showTaskReceiver, intentFilter);
     }
 
     //------------------------------------------------------------------------------------------------------------------
     @Override
-    protected void onStart()
-    //                                                      //Called when the activity is about to become visible.
-    {
-        super.onStart();
-        Log.d(tag, "The onStart() event");
-    }
-    //------------------------------------------------------------------------------------------------------------------
-    @Override
     protected void onResume()
-    //                                                      //Called when the activity has become visible.
+    //                                                  //Called when the activity has become visible.
     {
         super.onResume();
-        Log.d(tag, "The onResume() event");
-        //                                                  //Asking to the DB for data.
         TaskDB taskDBInstance = TaskDB.getTaskDB(getApplicationContext());
         DBUtil.DBGetAllTask(taskDBInstance, getApplicationContext());
     }
@@ -64,47 +54,15 @@ public class MainActivity extends AppCompatActivity
     }
     //------------------------------------------------------------------------------------------------------------------
     @Override
-    protected void onStop()
-    //                                                      //Called when the activity is no longer visible.
-    {
-        super.onStop();
-        Log.d(tag, "The onStop() event");
-    }
-    //------------------------------------------------------------------------------------------------------------------
-    @Override
     public void onDestroy()
     //                                                      //Called just before the activity is destroyed.
     {
         super.onDestroy();
+        //                                                  //Debugging purpose.
         Log.d(tag, "The onDestroy() event");
         //                                                  //Destroy de DB INSTANCE.
         TaskDB.destroyInstance();
     }
-
-    //------------------------------------------------------------------------------------------------------------------
-
-    public void ShowNewTaskForm(View view)
-    //                                                      //onClick-ButtonNewTask (New button)
-    {
-        //                                                  //To verify the click get the method.
-        Log.d(tag, "click ButtonNewTask");
-
-        //                                                  //Explicit Intent to start NewTaskFormActivity.
-        Intent intent = new Intent(getApplicationContext(), NewTaskFormActivity.class);
-        startActivity(intent);
-    }
-    //------------------------------------------------------------------------------------------------------------------
-    public void ShowAllTasks(View view)
-    //                                                      //onClick-ButtonShowAll (Show All button)
-    {
-        //                                                  //To verify the click get the method.
-        Log.d(tag, "click ButtonShowAll");
-
-        //                                                  //Explicit Intent to start TaskListActivity.
-        Intent intent = new Intent(getApplicationContext(), TaskListActivity.class);
-        startActivity(intent);
-    }
-
     //------------------------------------------------------------------------------------------------------------------
     private class ShowTaskReceiver extends BroadcastReceiver {
         @Override
@@ -116,8 +74,26 @@ public class MainActivity extends AppCompatActivity
                 Log.d("LGF - Tasks ", task.getShortDescription() + ", " +
                         String.valueOf(task.getPercentage()));
             }
+
+
+            //                                                  //Referencing RecyclerView
+            RecyclerView recyclerView = findViewById(R.id.recycletViewTasks);
+
+            //                                                  //Setting the adapter for the RecyclerList
+            ListTaskAdapter listTaskAdapter = new ListTaskAdapter(listOfTask);
+            recyclerView.setAdapter(listTaskAdapter);
+
+            //                                                  //Setting the manager for the RecyclerList
+            LinearLayoutManager manager = new LinearLayoutManager(
+                    getApplicationContext(), LinearLayoutManager.VERTICAL, false);
+            recyclerView.setLayoutManager(manager);
+
         }
     }
+
+
+
     //------------------------------------------------------------------------------------------------------------------
+
 }
 /*END-ACTIVITY*/
